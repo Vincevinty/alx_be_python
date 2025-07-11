a9515c3c6112384d3a8671ade46ed553f0962b30
Vincevinty <vincevinty@gmail.com>
2025-07-11 23:46:38 +0200

    Wrote a Basic Python arithmetic script for variable assignments to calculate the area of a rectangle using its length and width

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+#Assign values to variables.
+length = 10
+width = 5
+#Calculating the area of the rectangle.
+area = length * width
+#Print the result.
+print("The area of the rectangle is:", area)
