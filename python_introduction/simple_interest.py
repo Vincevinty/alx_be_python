f7480296a6ff44751bb299bd3454b3ccf784b907
Vincevinty <vincevinty@gmail.com>
2025-07-11 23:22:03 +0200

    Wrote a Python script that calculates the simple interest earnedon an investment over time

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Assign values to variables.
+principal = 1000 
+rate = 0.05
+time = 3
+#Calculating simple interest.
+simple_interest = (principal * rate * time)
+#Print the result.
+print("The simple interest is:", simple_interest)
