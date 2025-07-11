3eecb17afed1492fbce0f649265880ff72ce075e
Vincevinty <vincevinty@gmail.com>
2025-07-12 00:50:35 +0200

    Python script calculating future age

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,14 @@
-#Assign values to variables.
-current_age = 30
+
+# Assign values to variables.
+while True:
+    try:
+        current_age = int(input("How old are you? "))
+        break
+    except ValueError:
+        print("Please enter a valid integer for your age.")
 future_years = 2050
 current_year = 2023
-#Calculating future age.
+# Calculating future age.
 future_age = current_age + (future_years - current_year)
-#Print the result.
+# Print the result.
 print(f"In {future_years}, you will be {future_age} years old.")
