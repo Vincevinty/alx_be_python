04111eaa4f85d48acdb8ed7b999263579f4755a6
Vincevinty <vincevinty@gmail.com>
2025-07-12 00:03:04 +0200

    Wrote a Python script that asks the user for their current age and then calculates how old they will be in a specific future year

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#Assign values to variables.
+current_age = 30
+future_year = 2050
+current_year = 2023
+#Calculating future age.
+future_age = current_age + (future_year - current_year)
+#Print the result.
+print(f"In the year {future_year}, you will be {future_age} years old.")
