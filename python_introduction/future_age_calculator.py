775c64698fdad61dd1828707ac656f78572ec80d
Vincevinty <vincevinty@gmail.com>
2025-07-12 00:07:40 +0200

    Modified script for correct User Input Prompt

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -3,6 +3,6 @@ current_age = 30
 future_year = 2050
 current_year = 2023
 #Calculating future age.
-future_age = current_age + (future_year - current_year)
+How_old_are_you = current_age + (future_year - current_year)
 #Print the result.
-print(f"In the year {future_year}, you will be {future_age} years old.")
+print(f"In the year {future_year}, you will be {How_old_are_you} years old.")
