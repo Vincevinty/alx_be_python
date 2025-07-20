f870db43b0adbd9e75c0112ecee8a91f4012e12f
Vincevinty <vincevinty@gmail.com>
2025-07-21 00:22:00 +0200

    modified my script to convert input to lower case once

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,15 @@
 #Ask for user input for weather conditions
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
+#Convertinput to lower case once
+weather = weather.lower()
 # Provide advice based on the weather conditions
-if weather.lower() == "sunny":
+if weather == "sunny":
 #sunny: Wear a t-shirt and sunglasses.
     print("Wear a t-shirt and sunglasses.")
-elif weather.lower() == "rainy":
+elif weather == "rainy":
 #rainy: Don't forget your umbrella and a raincoat.
     print("Don't forget your umbrella and a raincoat.")
-elif weather.lower() == "cold":
+elif weather == "cold":
 #cold: Make sure to wear a warm coat and a scarf.
     print("Make sure to wear a warm coat and a scarf.")
 else:
