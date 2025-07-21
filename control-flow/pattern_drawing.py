4e260ef90ef403f20bd7bd2665b9fbf9bfc5fd05
Vincevinty <vincevinty@gmail.com>
2025-07-21 22:30:47 +0200

    Developed a Python script that prompts user to enter a positive integer, then use nested loops to print a square pattern of that size made of asterisks

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,18 @@
+# This script is part of a pattern drawing module.
+# Prompt user to enter positive integer that represents the size of the pattern.
+size = int(input("Enter size of the pattern: "))
+
+# Initialize row counter
+current_row = 0
+
+# While loop to draw the pattern
+while current_row < size:
+    
+
+# For loop for columns in the current row
+    for current_column in range(size):
+        print("*", end="")
+    print()  # Move to the next line after printing all columns in the current row
+    current_row += 1  # Increment the row counter
+
+
