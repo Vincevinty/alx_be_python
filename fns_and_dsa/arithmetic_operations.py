7e310a1a818d48774960e94bbb47c67aafb104cc
Vincevinty <vincevinty@gmail.com>
2025-07-27 21:01:29 +0200

    Removed type hints, to pass checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 # A function that performs basic arithmetic operations
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Executes arithmetic operations.
 
