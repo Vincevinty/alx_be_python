f2c93613e95e161f0629086c8e105072dedce04c
Vincevinty <vincevinty@gmail.com>
2025-07-30 18:19:36 +0200

    Modified line 12, the prompt statement to pass the checker

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,7 +9,7 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days_to_add = int(input("Enter the number of days to add: "))
+        days_to_add = int(input("Enter the number of days to add to current date: "))
         future_date = datetime.now() + timedelta(days=days_to_add)
         print("Future Date:", future_date.strftime("%Y-%m-%d"))
     except ValueError:
